00377d922a4204d4889641191658779cc151091e
Yury Gaydaychuk <agri1987cola@gmail.com>
2018-11-24 19:16:27 +0300

    19 16

--- modules/task_3/Gaidaichuk_Yuri_task3_Monte_Carlo/main.cpp
+++ modules/task_3/Gaidaichuk_Yuri_task3_Monte_Carlo/main.cpp
@@ -10,7 +10,7 @@
 #define rightBoard 1
 
 double givenFunction(double a1) {
-  return a1;
+  return a1*a1;
 }
 
 double myRand(double fMin, double fMax)
@@ -19,18 +19,19 @@ double myRand(double fMin, double fMax)
     return fMin + f * (fMax - fMin);
 }
 
-double sequenceMonteCarloIntegral(int setPower){
+double sequenceMonteCarloSum(int setPower){
   int seed = static_cast<int>(MPI_Wtime());
   std::srand(seed);
   double sum = 0;
   for(int i = 0; i < setPower; i++) {
     double randomArg = myRand(leftBoard, rightBoard);
-    sum += givenFunction(randomArg);
+    double part = givenFunction(randomArg);
+    sum += part;
   }
-	return sum / setPower;
+	return sum;
 }
 
-double partialMonteCarloIntegral(int partialSetPower, int rank){
+double partialMonteCarloSum(int partialSetPower, int rank){
   // initialize specific seed
   int seed = static_cast<int>(MPI_Wtime()) * rank;
   std::srand(seed);
@@ -43,13 +44,11 @@ double partialMonteCarloIntegral(int partialSetPower, int rank){
 }
 
 int main(int argc, char * argv[]) {
-  // TODO вставить борд
   int status = 0, rank = 0, procNum = 0;
   int setPower = atoi(argv[1]);
   double localMonteCarloIntegral = 0;
   double globalMonteCarloIntegral = 0;
   double parTime1 = 0, parTime2 = 0, seqTime1 = 0, seqTime2 = 0;
-  // double starttime2, endtime2, starttime1, endtime1, starttime3, endtime3;
 
   status = MPI_Init(& argc, & argv);
   // assert(status == MPI_SUCCESS);
@@ -75,18 +74,21 @@ int main(int argc, char * argv[]) {
   // чтобы были разные наборы
   MPI_Barrier(MPI_COMM_WORLD);
   parTime1 = MPI_Wtime();
-  localMonteCarloIntegral = partialMonteCarloIntegral(setPower / procNum, rank);
+  localMonteCarloIntegral = partialMonteCarloSum(setPower / procNum, rank);
   MPI_Reduce(&localMonteCarloIntegral, &globalMonteCarloIntegral, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
   parTime2 = MPI_Wtime();
   if (rank == 0) {
-    std::cout << "Parallel result = " << globalMonteCarloIntegral << "\n";
-    std::cout << "Time consumed = "<< parTime2 - parTime1 << " msc\n";
+    std::cout << "Parallel result = " << globalMonteCarloIntegral / setPower << "\n";
+    std::cout << "Time consumed = "<< parTime2 - parTime1 << " sec\n";
     seqTime1 = MPI_Wtime();
-    std::cout << "Sequence result = " << sequenceMonteCarloIntegral(setPower) << "\n";
+    std::cout << "Sequence result = " << sequenceMonteCarloSum(setPower) / setPower << "\n";
     seqTime2 = MPI_Wtime();
-    std::cout << "Time consumed = "<< seqTime2 - seqTime1 << " msc\n";
+    std::cout << "Time consumed = "<< seqTime2 - seqTime1 << " sec\n";
   }
   status = MPI_Finalize();
   assert(status == MPI_SUCCESS);
   return 0;
 }
+// исправить последовательный 
+// обновить трэвис
+// залить
