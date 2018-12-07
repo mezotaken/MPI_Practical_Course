17169d43bc31c267b739cd7f8fd57fa64ed6d16a
ivanvikhrev <laind4471@gmail.com>
2018-12-08 02:16:33 +0300

    fix3

--- modules/task_3/Vikhrev_Ivan_task3_radix_sort/main.cpp
+++ modules/task_3/Vikhrev_Ivan_task3_radix_sort/main.cpp
@@ -8,19 +8,19 @@
 
 #define MainProc 0
 
-void swap(uint32_t *a, uint32_t *b) {
-    uint32_t tmp = *a;
+void swp(unsigned int *a, unsigned int *b) {
+    unsigned int tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-uint32_t* merge(uint32_t *arr1, uint32_t num1, uint32_t *arr2, uint32_t num2) {
-    uint32_t *res;
-    uint32_t i = 0,
+unsigned int* merge(unsigned int *arr1, unsigned int num1, unsigned int *arr2, unsigned int num2) {
+    unsigned int *res;
+    unsigned int i = 0,
         j = 0,
         index = 0;
 
-    res = new uint32_t[num1 + num2];
+    res = new unsigned int[num1 + num2];
 
     while (i < num1 && j < num2) {
         if (arr1[i] < arr2[j])
@@ -37,16 +37,16 @@ uint32_t* merge(uint32_t *arr1, uint32_t num1, uint32_t *arr2, uint32_t num2) {
     return res;
 }
 
-void rad_sort_1(uint32_t *from, uint32_t *to, uint32_t bit) {
+void rad_sort_1(unsigned int *from, unsigned int *to, unsigned int bit) {
     if (!bit || to < from + 1) return;
 
-    uint32_t *ll = from, *rr = to - 1;
+    unsigned int *ll = from, *rr = to - 1;
 
     for (;;) {
         while (ll < rr && !(*ll & bit)) ll++;
         while (ll < rr && (*rr & bit)) rr--;
         if (ll >= rr) break;
-        swap(ll, rr);
+        swp(ll, rr);
     }
 
     if (!(bit & *ll) && ll < to) ll++;
@@ -56,11 +56,11 @@ void rad_sort_1(uint32_t *from, uint32_t *to, uint32_t bit) {
     rad_sort_1(ll, to, bit);
 }
 
-void rad_sort_2(uint32_t * a, size_t count) {
-    size_t mIndex[4][256] = { 0 };  // count / index matrix
-    uint32_t *b = new uint32_t[count];  // allocate temp array
-    size_t i, j, m, n;
-    uint32_t u;
+void rad_sort_2(unsigned int * a, unsigned int count) {
+	unsigned int mIndex[4][256] = { 0 };  // count / index matrix
+    unsigned int *b = new unsigned int[count];  // allocate temp array
+	unsigned int i, j, m, n;
+    unsigned int u;
     for (i = 0; i < count; i++) {
         u = a[i];
         for (j = 0; j < 4; j++) {
@@ -86,9 +86,9 @@ void rad_sort_2(uint32_t * a, size_t count) {
     }
     delete[] b;
 }
-bool is_sorted(uint32_t* arr, uint32_t size) {
+bool is_sorted(unsigned int* arr, unsigned int size) {
     bool flag = 1;
-    for (uint32_t i = 0; i < size - 1; i++)
+    for (unsigned int i = 0; i < size - 1; i++)
         if (arr[i] > arr[i + 1]) {
             flag = 0;
             break;
@@ -100,8 +100,8 @@ int main(int argc, char** argv) {
     int status = 0, rank = 0, size = 0;  // MPI vars
     int *scounts = NULL,  // use in scatterv
         *displs = NULL;  // use in scatterv
-    uint32_t arrSize = 0;
-    uint32_t  *arr = NULL,  // data
+    unsigned int arrSize = 0;
+    unsigned int  *arr = NULL,  // data
               *buff = NULL,  // buffers for message exchanging
               *buff2 = NULL,
               *resultS = NULL,  // results
@@ -133,14 +133,14 @@ int main(int argc, char** argv) {
             type_sort = 0;
           }
         std::cout << "  Array size : " << arrSize << std::endl;
-        arr = new uint32_t[arrSize];
-        resultS = new uint32_t[arrSize];
-        resultP = new uint32_t[arrSize];
+        arr = new unsigned int[arrSize];
+        resultS = new unsigned int[arrSize];
+        resultP = new unsigned int[arrSize];
         std::srand((unsigned)time(NULL));
-        for (uint32_t i = 0; i < arrSize; i++)
+        for (unsigned int i = 0; i < arrSize; i++)
             arr[i] = resultS[i] = std::rand() % 201;
         if (arrSize < 50) {
-            for (uint32_t i = 0; i < arrSize; i++)
+            for (unsigned int i = 0; i < arrSize; i++)
                 std::cout << arr[i] << " ";
             std::cout << std::endl;
         }
@@ -156,12 +156,12 @@ int main(int argc, char** argv) {
         else
             std::cout << "    Array isn`t sorted, flag = " << f << std::endl;
         if (arrSize < 50) {
-            for (uint32_t i = 0; i < arrSize; i++)
+            for (unsigned int i = 0; i < arrSize; i++)
                 std::cout << resultS[i] << " ";
             std::cout << std::endl;
         }
-        buff = new uint32_t[arrSize / size + arrSize % size];
-        buff2 = new uint32_t[arrSize / size ];
+        buff = new unsigned int[arrSize / size + arrSize % size];
+        buff2 = new unsigned int[arrSize / size ];
     }
     MPI_Barrier(MPI_COMM_WORLD);
     t1 = MPI_Wtime();
@@ -176,7 +176,7 @@ int main(int argc, char** argv) {
         displs[i] = scounts[0] + (i-1)*scounts[i];
     }
     if (rank != 0)
-        buff = new uint32_t[scounts[rank]];
+        buff = new unsigned int[scounts[rank]];
 
     MPI_Scatterv(arr, scounts, displs, MPI_UNSIGNED, buff,
         scounts[rank], MPI_UNSIGNED, MainProc, MPI_COMM_WORLD);
@@ -192,9 +192,9 @@ int main(int argc, char** argv) {
 
 
     if (rank == 0) {
-        for (uint32_t i = 0; i < static_cast<uint32_t>(scounts[0]); i++)
+        for (unsigned int i = 0; i < static_cast<unsigned int>(scounts[0]); i++)
             resultP[i] = buff[i];
-        for (uint32_t i = 1; i < static_cast<uint32_t>(size); i++) {
+        for (unsigned int i = 1; i < static_cast<unsigned int>(size); i++) {
             MPI_Recv(buff2, arrSize / size, MPI_UNSIGNED, MPI_ANY_SOURCE,
                 i, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
             resultP = merge(buff2, arrSize / size, resultP,
@@ -207,7 +207,7 @@ int main(int argc, char** argv) {
         else
             std::cout << "    Array isn`t sorted, flag = " << f << std::endl;
         if (arrSize < 50) {
-            for (uint32_t i = 0; i < arrSize; i++)
+            for (unsigned int i = 0; i < arrSize; i++)
                 std::cout << resultP[i] << " ";
             std::cout << std::endl;
         }
